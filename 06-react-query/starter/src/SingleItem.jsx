ed421c105fe1f50ce47e07907a1674df18e9bda3
Lam Vo <vo2022@protonmail.com>
2023-05-12 12:58:35 -0700

    added edit item and delete item

--- 06-react-query/starter/src/SingleItem.jsx
+++ 06-react-query/starter/src/SingleItem.jsx
@@ -1,34 +1,49 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const SingleItem = ({ item }) => {
-const {mutate: editTask} = useMutation({
+  const queryCLient = useQueryClient();
 
-    mutationFn: ({taskId, isDone}) => {
-      return customFetch.patch(`/${taskId}`, {isDone})
-    }
-})
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => 
+       customFetch.patch(`/${taskId}`, { isDone }),
+    onSuccess: () => {
+      queryCLient.invalidateQueries("tasks");
+      toast.success("task edited");
+    }, 
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: ({ taskId}) => 
+       customFetch.delete(`/${taskId}`),
+    onSuccess: () => {
+      queryCLient.invalidateQueries("tasks");
+      toast.success("task deleted");
+    }, 
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
   return (
-    <div className='single-item'>
-      <input
-        type='checkbox'
-        checked={item.isDone}
-        onChange={() => editTask}
-      />
+    <div className="single-item">
+      <input type="checkbox" checked={item.isDone} onChange={() => editTask({taskId:item.id, isDone:!item.isDone})} />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
+        className="btn remove-btn"
+        type="button"
+        onClick={() => deleteTask({taskId:item.id})}
       >
         delete
       </button>
